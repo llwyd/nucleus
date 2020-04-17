832c8cc11f6a90c080ccce96db1e34cac23f173f
llwyd <tom@llwyd.io>
2020-04-17 21:11:05 +0100

    basic graph plotting

--- homepage/app.py
+++ homepage/app.py
@@ -8,7 +8,7 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import plotly
 from dash.dependencies import Input, Output
-
+import numpy as np
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -20,9 +20,7 @@ app.title='home Assistant v 0.0.1'
 app.server.config["DEBUG"] = True
 
 
-#------------------------------------------------------------------------------------------------------------------
 #   Entry point for the website
-#------------------------------------------------------------------------------------------------------------------
 app.layout = html.Div(children=[
 
     dcc.Location(id='url', refresh=False),
@@ -30,23 +28,51 @@ app.layout = html.Div(children=[
 ])
 
 
-#------------------------------------------------------------------------------------------------------------------
 #   URL Handler
-#------------------------------------------------------------------------------------------------------------------
 @app.callback(Output('page_content', 'children'),[Input('url', 'pathname')])
 def display_page(pathname):
     return index_page
 
-#------------------------------------------------------------------------------------------------------------------
 #   Static Index Page
-#------------------------------------------------------------------------------------------------------------------
 index_page = html.Div([
     html.H1(children='Home Assistant Version 0.1'),
     html.H3(children='Temperature Data'),
-    html.H3(children='Server Info')
-    #dcc.Graph(id='static_temp_graph')
+    html.H3(children='Server Info'),
+    dcc.Dropdown(   id='date-dropdown',
+                    style={'height':'35px','width':'200px'},
+                    options=[
+                                {'label':'test','value':'1234'},
+                            ],
+                    clearable=False,
+                    placeholder="Select a data"
+                    #value = 0
+    ),
+    dcc.Graph(id='static-temp-graph')
 ])
 
+#   Temperature graph
+@app.callback(Output('static-temp-graph', 'figure'),[Input('date-dropdown','value')])
+def static_temp_graph(value):
+    data={}
+    print("Hello")
+    data['y']=np.random.rand(1000);
+    data['x']=np.linspace(0,999,1000);
+    figure={
+        'data': [
+            {'x': data['x'], 'y': data['y'], 'type': 'scatter', 'name': 'Noise'}
+        ],
+        'layout': {
+            'title': 'Noise',
+            'xaxis': {
+                'title': 'Time (Seconds)'
+            },
+            'yaxis': {
+                'title': 'Amplitude'
+            }
+        },
+
+    }
+    return figure;
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0');
