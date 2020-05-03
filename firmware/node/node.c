6852f7409a00371afe68fc069b31b25a2a9d04b9
llwyd <tom@llwyd.io>
2020-05-03 15:33:21 +0100

    successful i2c reading

--- firmware/node/node.c
+++ firmware/node/node.c
@@ -5,18 +5,37 @@
 #include "user_interface.h"
 #include "espconn.h"
 #include "mem.h"
+#include "driver/i2c_master.h"
 #include "user_config.h"
 
 //Define Timer
-static volatile os_timer_t t;
+static  os_timer_t t;
 
 // define station config struct
 static struct station_config stat;
 
+static char addr = 0x48;
+
+static char data[2] = {0,0};
+
 /* Timer */
 void timer(void *arg)
 {
-    	os_printf("Hello, World\n");
+	i2c_master_start();
+	i2c_master_writeByte( 0x91 );
+	if(i2c_master_checkAck())
+	{
+		data[0] = i2c_master_readByte();
+		i2c_master_send_ack();
+		data[1] = i2c_master_readByte();
+		i2c_master_send_nack();
+		i2c_master_stop();
+		os_printf("%x,%x\n",data[0],data[1]);
+	}
+	else
+	{
+		os_printf("I2C Error\n");
+	}
 }
 
 /* Setup WiFi connection */
@@ -40,9 +59,14 @@ void ICACHE_FLASH_ATTR user_init()
 {
   	// init gpio
   	gpio_init();
+	
 	// Allow uart output for debug
 	uart_div_modify(0, UART_CLK_FREQ / 115200);
-  	//Setup/Connect WIFI
+	
+	/* Initialise I2C */
+	i2c_master_gpio_init();
+  	os_delay_us(10000);	
+	//Setup/Connect WIFI
   	Node_WifiSetup();
   	//Timer Setup
   	os_timer_setfn(&t, (os_timer_func_t *)timer, NULL);
