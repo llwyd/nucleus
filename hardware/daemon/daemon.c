3018e1d64bb028a8d84536ab5ce6bd3a03f87f42
llwyd <tom@llwyd.io>
2020-08-22 13:33:15 +0100

    sequential task queue/state machine working successfully

--- hardware/daemon/daemon.c
+++ hardware/daemon/daemon.c
@@ -13,29 +13,14 @@
 
 typedef enum state_t
 {
-	state_Initialise,
 	state_ReadTemp,
 	state_ReadWeather,
-	state_Delay,
 	state_SendData,
-	state_ReceiveData,
-	state_Exit,
+	state_RcvData,
+	/*---------*/
+	state_Count,
 } state_t;
 
-typedef union data_t
-{
-	float f;
-	int i;
-	uint8_t * c;
-} data_t;
-
-typedef enum
-{
-	data_Float,
-	data_int16,
-	data_Char,
-	data_NULL,
-} data_type_t;
 
 typedef struct state_data_t
 {
@@ -45,57 +30,86 @@ typedef struct state_data_t
 	/* Sensor Data*/
 	float temperature;	/* Sensor Temperature */
 	float humidity;		/* Sensor Humidity */
+
+	/* Weather Description */
+	uint8_t * weather;
 } state_data_t;
 
-
-void (*state_fn)( state_data_t *data, float);
-
-static time_t stateTime;
+typedef struct
+{
+	state_t currentState;					/* Current State Enumeration */	
+	void (*state_fn)( state_data_t *data);	/* fn pointer */
+	double time;							/* how often to execute */
+	time_t currentTime;
+} state_func_t;
 
 /* State Prototypes */
-void State_Init( state_data_t * data, float delay);
+void State_Init( state_data_t * data );
 void State_ReadTemp( state_data_t * data );
 void State_ReadWeather( state_data_t * data );
-void State_Delay( state_data_t * data );
-void State_Exit( state_data_t * data, float delay );
+void State_SendData( state_data_t * data );
+void State_RcvData( state_data_t * data );
 
-
-void State_Init( state_data_t * data, float delay)
+state_func_t StateTable[] = 
 {
-	time_t currentTime;
-	time( &currentTime );
+	{state_ReadTemp,	State_ReadTemp, 	5,0},
+	{state_ReadWeather,	State_ReadWeather,	5,0},
+	{state_SendData,	State_SendData,		5,0},
+	{state_RcvData,		State_RcvData,		1,0},
+};
 
-	float delta = difftime( currentTime, stateTime );
-
-	if( delta > delay )
-	{
-		printf("Time Elapsed\n");
-		time( &stateTime );
-		state_fn = State_Exit;
-	}
-	else
-	{
-		state_fn = State_Init;
-	}
-}
-
-void State_Exit( state_data_t *data, float delay)
+void State_Init( state_data_t * data )
 {
-	printf("FIN\n");
+	printf("Initial State\n");
 }
-
+void State_ReadTemp( state_data_t * data )
+{
+	printf("ReadTemp State\n");
+}
+void State_ReadWeather( state_data_t * data )
+{
+	printf("ReadWeather State\n");
+}
+void State_SendData( state_data_t * data )
+{
+	printf("SendData State\n");
+}
+void State_RcvData( state_data_t * data )
+{
+	printf("RcvData State\n");
+}
 
 uint8_t main( int16_t argc, uint8_t **argv )
 {
-	state_data_t s;
-	time( &stateTime );
+	/* Data Structure for passing data between states */
+	static state_data_t s;
 
-	state_fn = State_Init;
-	while(state_fn != State_Exit)
+	/* Master time struct for running states at set periods*/
+	time_t stateTime;
+
+	/* Initialise all the state time structures*/
+	for(int i=0;i<state_Count;i++)
 	{
-		state_fn(&s, 5.0f);
+		time(&StateTable[i].currentTime);
 	}
 	
+	/* Inf Loop */
+	while (1)
+	{
+		/* Iterate through all the states sequentially */
+		for( int i=0; i < state_Count; i++)
+		{
+			
+			time(&stateTime);
+			double delta = difftime(stateTime, StateTable[i].currentTime );
+			
+			if( delta > StateTable[i].time )
+			{
+				StateTable[i].state_fn( &s );
+				StateTable[i].currentTime = stateTime;
+			}
+		}
+	}
 
 	return 0;
 }
