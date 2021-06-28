e929ba9160ac6a4c12e87f60f3b492fe7823d6b2
llwyd <tom@llwyd.io>
2021-06-28 23:10:28 +0100

    initialisation of BME280 successful using the bosch sdk

--- firmware/node32/main/sensing.c
+++ firmware/node32/main/sensing.c
@@ -9,6 +9,7 @@
 
 #include "stdio.h"
 
+#include "bme280.h"
 
 /* current temperature value */
 static float temperature = 0.0f;
@@ -20,6 +21,7 @@ static int i2c_master_port = 0U;
 static int taskDelay = 1000;
 
 static float TMP102_Read( void );
+static void  BME280_Setup( void );
 
 static QueueHandle_t * xDataQueue;
 static QueueHandle_t * xSlowDataQueue;
@@ -42,6 +44,8 @@ void Sensing_Init( QueueHandle_t * xTemperature, QueueHandle_t * xSlowTemperatur
 
     xDataQueue = xTemperature;
     xSlowDataQueue = xSlowTemperature;
+
+    BME280_Setup();
 }
 
 extern void Sensing_Task( void * pvParameters )
@@ -102,5 +106,90 @@ static float TMP102_Read( void )
     currentTemperature = ( ( float )rawTemperature ) * scaling;
 
     return currentTemperature;
+}
+
+void BME280_Delay(uint32_t period, void *intf_ptr)
+{
+    ets_delay_us( period );
+}
+
+int8_t BME280_I2CRead(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr)
+{
+        int8_t rslt = 0;
+        uint8_t address = *(uint8_t *)intf_ptr;
+
+        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+        i2c_master_start( cmd );
+        i2c_master_write_byte( cmd, ( address << 1) & 0xFE, 0x1 );
+        i2c_master_write_byte( cmd, reg_addr, 0x1 );
+
+        i2c_master_start( cmd );
+        i2c_master_write_byte( cmd, ( address << 1) | 0x1, 0x1 );
+
+        if( len > 1 )
+        {
+            i2c_master_read( cmd, reg_data, len - 1, 0x0 );
+        }
+
+        i2c_master_read_byte( cmd, reg_data + len - 1, 0x1 );
+        i2c_master_stop( cmd );
+
+        esp_err_t ret = i2c_master_cmd_begin( i2c_master_port, cmd, 1000 / portTICK_PERIOD_MS );
+        i2c_cmd_link_delete(cmd);
+
+        if( ret != ESP_OK )
+        {
+            printf("BME280 READ FAIL\n");
+            rslt = -1;
+        }
+
+        return rslt;
+}
+
+
+int8_t BME280_I2CWrite(uint8_t reg_addr, const uint8_t *reg_data, uint32_t len, void *intf_ptr)
+{
+        int8_t rslt = 0;
+        uint8_t address = *(uint8_t *)intf_ptr;
+
+        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+        i2c_master_start( cmd );
+        i2c_master_write_byte( cmd, ( address << 1) & 0xFE, 0x1 );
+        i2c_master_write_byte( cmd, reg_addr, 0x1 );
+        i2c_master_write( cmd, reg_data, len, 0x1 );
+        i2c_master_stop( cmd );
+
+        esp_err_t ret = i2c_master_cmd_begin( i2c_master_port, cmd, 1000 / portTICK_PERIOD_MS );
+        i2c_cmd_link_delete(cmd);
+
+        if( ret != ESP_OK )
+        {
+            printf("BME280 WRITE FAIL\n");
+            rslt = -1;
+        }
+        return rslt;
+}
+
+static void BME280_Setup( void )
+{
+    struct bme280_dev dev;
+    int8_t rslt = BME280_OK;
+    uint8_t dev_addr = BME280_I2C_ADDR_PRIM;
+
+    dev.intf_ptr    = &dev_addr;
+    dev.intf        = BME280_I2C_INTF;
+    dev.read        = BME280_I2CRead;
+    dev.write       = BME280_I2CWrite;
+    dev.delay_us    = BME280_Delay;
+
+    rslt = bme280_init(&dev);
+    if( rslt != BME280_OK )
+    {
+        printf("BME280 Init FAIL\n");
+    }
+    else
+    {
+        printf("BME280 Init OK\n");
+    }
 }
 
