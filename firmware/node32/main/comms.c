a38f70b37e4c169e0516f6cc2371a37dc72280b7
llwyd <tom@llwyd.io>
2021-12-18 17:14:26 +0000

    moved mqtt functionality into separate file

--- firmware/node32/main/comms.c
+++ firmware/node32/main/comms.c
@@ -25,31 +25,19 @@
 
 #include "comms.h" 
 #include "dataqueue.h"
-#include "mqtt_client.h"
+#include "mqtt.h"
 #include "secretkeys.h"
 #include "types.h"
 
-#define MQTT_PIN ( 13U )
 
 static EventGroupHandle_t s_wifi_event_group;
 static const char * WIFI_TAG = "station";
 
-static void MQTT_Init( void );
- 
-static esp_mqtt_client_handle_t mqtt_client;
-
-static bool brokerConnected = false;
 static bool wifiConnected = false;
 
 static void ExtractAndTransmit( QueueHandle_t * queue, char * buffer );
-static void MQTT_ReceiveTest ( dq_val_t * data );
-static void MQTT_HandleReceivedData( const char * const topic, const uint32_t topic_len, const char * const data, const uint32_t data_len );
-
-static uint16_t numCallbacks = 1U;
-static dq_callback_t mqttCallbacks [ 1 ] = 
-{
-    { "debug_led", dq_data_bool, MQTT_ReceiveTest },
-};
+static void ExtractFloatData( const char * inputBuffer, float * outputValue, const char * keyword );
+static void ExtractStringData( const char * inputBuffer, char * outputBuffer, const char * keyword );
 
 extern bool Comms_WifiConnected( void )
 {
@@ -79,11 +67,6 @@ static void Wifi_EventHandler( void * arg, esp_event_base_t event_base, int32_t 
 
 static void Init( void )
 {
-    /* DEBUG led for testing mqtt receive callbacks */
-    gpio_reset_pin( MQTT_PIN );
-    gpio_set_direction( MQTT_PIN, GPIO_MODE_OUTPUT );
-    gpio_set_level( MQTT_PIN, false );
-
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
     {
@@ -131,151 +114,6 @@ static void Init( void )
     esp_wifi_start();
 }
 
-static void MQTT_ReceiveTest( dq_val_t * data )
-{
-    printf("MQTT: Received data\n");
-    gpio_set_level( MQTT_PIN, data->b );
-}
-
-static dq_val_t MQTT_Extract( char * data, dq_type_t type )
-{
-    dq_val_t d;
-
-    switch( type )
-    {
-        case dq_data_float:
-            d.f = (float)atof(data);
-            break;
-        case dq_data_uint32:
-            d.ui = (uint32_t)atoi(data);
-            break;
-        case dq_data_uint16:
-            d.us = (uint16_t)atoi(data);
-            break;
-        case dq_data_str:
-            d.s = data;
-            break;
-        case dq_data_bool:
-            if( strcmp( "1", data ) == 0U )
-            {
-                d.b = true;
-            }
-            else
-            {
-                d.b = false;
-            }
-            break;
-        default:
-        {
-            assert( false );
-            break;
-        }
-    }
-    return d;
-}
-
-static void MQTT_HandleReceivedData( const char * const topic, const uint32_t topic_len, const char * const data, const uint32_t data_len )
-{
-    assert( topic != NULL );
-    assert( data != NULL );
-
-    char local_topic[64];
-    char recvd_topic[64];
-    char recvd_data [32];
-    memset( recvd_topic, 0x00, 64);
-    memset( recvd_data, 0x00, 32);
-
-    snprintf( recvd_topic, 64, "%.*s", topic_len, topic );
-    snprintf( recvd_data, 64, "%.*s", data_len, data );
-    
-    for( uint16_t idx = 0U; idx < numCallbacks; idx++ )
-    {
-        memset( local_topic, 0x00, 64);
-        strcat( local_topic, MQTT_MASTER_TOPIC );
-        strcat( local_topic, mqttCallbacks[idx].mqtt_label );
-        
-        if( strcmp( local_topic, recvd_topic ) == 0U )
-        {
-            dq_val_t data = MQTT_Extract( recvd_data, mqttCallbacks[idx].type );
-            mqttCallbacks[idx].dq_fn( &data );
-        }
-    }
-}
-
-static void MQTT_EventHandler( void * arg, esp_event_base_t event_base, int32_t event_id, void* event_data )
-{
-    esp_mqtt_event_handle_t event = event_data;
-    esp_mqtt_client_handle_t client = event->client;
-    esp_mqtt_event_id_t currentEvent = (esp_mqtt_event_id_t)event_id;
-
-    /* TODO, improve this */
-    switch( currentEvent )
-    {
-        case MQTT_EVENT_CONNECTED:
-            printf("Connected to broker!\n");
-            brokerConnected = true;
-            esp_mqtt_client_subscribe( client, MQTT_TOPIC, 0 );
-            break;
-        case MQTT_EVENT_DISCONNECTED:
-            brokerConnected = false;
-            printf("Disconnected from broker\n");
-            break;
-        case MQTT_EVENT_DATA:
-            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-            printf("DATA=%.*s\r\n", event->data_len, event->data);
-            MQTT_HandleReceivedData( event->topic, event->topic_len, event->data, event->data_len);
-            break;
-        default:
-            printf("Unhandled event\n");
-            break;
-    }
-
-}
-
-static void ConstructMQTTPacket( dq_data_t * data, char * topic, uint16_t topicSize, char * dataBuffer, uint16_t dataBufferSize )
-{
-    assert( data != NULL );
-    assert( topic != NULL );
-    assert( dataBuffer != NULL );
-    
-    memset( topic, 0x00, topicSize );
-    memset( dataBuffer, 0x00, dataBufferSize );
-    
-    strcat( topic, MQTT_MASTER_TOPIC );
-    
-    switch( data->type )
-    { 
-        case dq_data_float:
-        {
-            snprintf( dataBuffer, 16U, "%.3f", data->data.f);
-            strcat( topic, data->mqtt_label );
-            break;
-        }
-        case dq_data_uint32:
-        {
-            snprintf( dataBuffer, 16U, "%d", data->data.ui);
-            strcat( topic, data->mqtt_label );
-            break;
-        }
-        case dq_data_uint16:
-        {
-            snprintf( dataBuffer, 16U, "%d", data->data.us);
-            strcat( topic, data->mqtt_label );
-            break;
-        }
-        case dq_data_str:
-        {
-            strcat( dataBuffer, data->data.s);
-            strcat( topic, data->mqtt_label );
-            break;
-        }
-        default:
-        {
-            assert( false );
-        }
-    }    
-}
-
 extern void Comms_Task( void * pvParameters )
 {
     char mqttTopic[64] = {0x00};
@@ -285,34 +123,21 @@ extern void Comms_Task( void * pvParameters )
     QueueHandle_t * sensorQueue = (QueueHandle_t *)pvParameters;
 
     Init();
-
+    esp_mqtt_client_handle_t * mqtt = MQTT_GetClient();
     while( 1U )
     {
         if( xQueueReceive( *sensorQueue, &sensorData, (TickType_t)0 ) == pdPASS )
         {
-            ConstructMQTTPacket( &sensorData, mqttTopic, 64U, dataString, 32U );
+            MQTT_ConstructPacket( &sensorData, mqttTopic, 64U, dataString, 32U );
             
-            if( brokerConnected )
+            if( MQTT_BrokerConnected() )
             {
+                assert( mqtt != NULL );
                 printf("transmitting: %s\n", mqttTopic);
-                esp_mqtt_client_publish(mqtt_client, mqttTopic, dataString, 0, 0, 0);
+                esp_mqtt_client_publish( *mqtt, mqttTopic, dataString, 0, 0, 0);
             }
         } 
     } 
-}
-
-static void MQTT_Init( void )
-{
-    esp_mqtt_client_config_t mqtt_cfg =
-    {
-        .host = MQTT_BROKER,
-        .port = 1883,
-        .transport = MQTT_TRANSPORT_OVER_TCP,
-    };
-
-    mqtt_client = esp_mqtt_client_init( &mqtt_cfg );
-    esp_mqtt_client_register_event( mqtt_client, ESP_EVENT_ANY_ID, MQTT_EventHandler, NULL );
-    esp_mqtt_client_start(mqtt_client);
 }
 
 extern esp_err_t Comms_HTTPEventHandler( esp_http_client_event_t *evt )
