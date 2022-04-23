2dc40cae320a3dd956656e2a2c6c67852e766e3a
llwyd <tom@llwyd.io>
2022-04-23 18:31:29 +0100

    basic display of temperature working

--- display/inkdisplay.py
+++ display/inkdisplay.py
@@ -1,6 +1,10 @@
 import argparse
 import paho.mqtt.client as mqtt
 import time
+from inky.auto import auto
+from PIL import Image, ImageFont, ImageDraw
+from font_fredoka_one import FredokaOne
+from font_hanken_grotesk import HankenGroteskBold
 
 rcv_lounge_temp = False
 rcv_lounge_hum = False
@@ -46,9 +50,19 @@ def on_message(client, userdata, message):
 
         client.loop_stop()
 
+# Initialise display
+display = auto()
+display.set_border(display.BLACK)
+print("Initialising Display...")
+print("Colour: " + str(display.colour) )
+print("   Res: " + str(display.resolution) )
+print("")
+
+# Init MQTT
 client = mqtt.Client()
 client.on_message = on_message
 
+# Get IP of broker via argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("ip", help = "IP address of MQTT broker")
 args = parser.parse_args()
@@ -58,5 +72,34 @@ broker_ip = str(args.ip)
 client.connect(broker_ip, 1883, 60)
 client.subscribe("home/#")
 
+print("Collecting Data via MQTT...")
 client.loop_start()
 time.sleep(5)
+
+# Output to display
+# Initialise canvas
+img = Image.new( "P", (display.WIDTH, display.HEIGHT) )
+draw = ImageDraw.Draw(img)
+
+# Init font
+font = ImageFont.truetype(HankenGroteskBold, 22)
+
+def AddText( d, xy, s, data, f ):
+    label = s
+    data_str = "{0:.1f}".format(float(data))+"C"
+    full_string = label + data_str
+    d.text( xy, full_string, display.BLACK, f )
+
+#draw.text( (0, 0),  "    Lounge:", display.YELLOW, font)
+
+AddText( draw, (0, 44), "    Lounge: ", lounge_temp, font )
+AddText( draw, (0, 66), "Bedroom: ", bedroom_temp, font )
+
+print("Updating display...")
+display.set_image(img)
+display.show()
+
+print("FIN")
+
+
+
