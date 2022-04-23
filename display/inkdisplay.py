0aa7f44999f75627140fbc3542c5cdf8654ff6b6
llwyd <tom@llwyd.io>
2022-04-23 18:56:18 +0100

    now takes the slow outside data

--- display/inkdisplay.py
+++ display/inkdisplay.py
@@ -10,11 +10,15 @@ rcv_lounge_temp = False
 rcv_lounge_hum = False
 rcv_bedroom_temp = False
 rcv_bedroom_hum = False
+rcv_outside_temp = False
+rcv_outside_hum = False
 
 lounge_temp = ""
 lounge_hum = ""
 bedroom_temp = ""
 bedroom_hum = ""
+outside_temp = ""
+outside_hum = ""
 
 def on_message(client, userdata, message):
     full_topic = message.topic
@@ -26,10 +30,15 @@ def on_message(client, userdata, message):
     global rcv_lounge_hum
     global rcv_bedroom_temp
     global rcv_bedroom_hum
+    global rcv_outside_temp
+    global rcv_outside_hum
+    
     global lounge_temp
     global lounge_hum
     global bedroom_temp
     global bedroom_hum
+    global outside_temp
+    global outside_hum
 
     if loc == 'lounge_area' and topic =='temp_live' and not rcv_lounge_temp:
         lounge_temp = data
@@ -43,10 +52,17 @@ def on_message(client, userdata, message):
     elif loc == 'bedroom_area' and topic == 'hum_live' and not rcv_bedroom_hum:
         bedroom_hum = data
         rcv_bedroom_hum = True
+    elif loc == 'lounge_area' and topic == 'out_temp' and not rcv_outside_temp:
+        outside_temp = data
+        rcv_outside_temp = True
+    elif loc == 'lounge_area' and topic == 'out_hum' and not rcv_outside_hum:
+        outside_hum = data
+        rcv_outside_hum = True
 
-    if( rcv_lounge_temp and rcv_bedroom_temp and rcv_lounge_hum and rcv_bedroom_hum ):
+    if( rcv_lounge_temp and rcv_bedroom_temp and rcv_lounge_hum and rcv_bedroom_hum and rcv_outside_temp and rcv_outside_hum ):
         print("  Lounge: ","{0:.1f}".format(float(lounge_temp)), "C ", "{0:.1f}".format(float(lounge_hum)),"%")
         print(" Bedroom: ","{0:.1f}".format(float(bedroom_temp)), "C ", "{0:.1f}".format(float(bedroom_hum)),"%")
+        print(" Outside: ","{0:.1f}".format(float(outside_temp)), "C ", "{0:.1f}".format(float(outside_hum)),"%")
 
         client.loop_stop()
 
@@ -74,7 +90,7 @@ client.subscribe("home/#")
 
 print("Collecting Data via MQTT...")
 client.loop_start()
-time.sleep(5)
+time.sleep(90)
 
 # Output to display
 # Initialise canvas
@@ -96,7 +112,7 @@ def AddText( d, xy,xy_data, s, temp, hum , f ):
 
 AddText( draw, (0, 0),  (45,16), "Lounge: ", lounge_temp, lounge_hum, font )
 AddText( draw, (0, 32), (45,48), "Bedroom: ", bedroom_temp, bedroom_hum, font )
-AddText( draw, (0, 64), (45,80), "Outside: ", "0.0", "0.0", font )
+AddText( draw, (0, 64), (45,80), "Outside: ", outside_temp, outside_hum, font )
 
 print("Updating display...")
 display.set_image(img)
