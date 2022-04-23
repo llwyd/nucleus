d7644f347594637aee9932320860b218740556e9
llwyd <tom@llwyd.io>
2022-04-23 18:01:49 +0100

    basic script that retrieves mqtt sensor data

--- display/inkdisplay.py
+++ display/inkdisplay.py
@@ -5,18 +5,58 @@ import time
 rcv_lounge_temp = False
 rcv_lounge_hum = False
 rcv_bedroom_temp = False
-rcv_bedroom_bum = False
+rcv_bedroom_hum = False
 
 lounge_temp = ""
 lounge_hum = ""
 bedroom_temp = ""
 bedroom_hum = ""
 
+def on_message(client, userdata, message):
+    full_topic = message.topic
+    topic = full_topic.replace('/',' ').split()[-1]
+    loc = full_topic.replace('/',' ').split()[-2]
+    data = message.payload.decode()
+    
+    global rcv_lounge_temp
+    global rcv_lounge_hum
+    global rcv_bedroom_temp
+    global rcv_bedroom_hum
+    global lounge_temp
+    global lounge_hum
+    global bedroom_temp
+    global bedroom_hum
+
+    if loc == 'lounge_area' and topic =='temp_live' and not rcv_lounge_temp:
+        lounge_temp = data
+        rcv_lounge_temp = True
+    elif loc == 'lounge_area' and topic == 'hum_live' and not rcv_lounge_hum:
+        lounge_hum = data
+        rcv_lounge_hum = True
+    elif loc == 'bedroom_area' and topic == 'temp_live' and not rcv_bedroom_temp:
+        bedroom_temp = data
+        rcv_bedroom_temp = True
+    elif loc == 'bedroom_area' and topic == 'hum_live' and not rcv_bedroom_hum:
+        bedroom_hum = data
+        rcv_bedroom_hum = True
+
+    if( rcv_lounge_temp and rcv_bedroom_temp and rcv_lounge_hum and rcv_bedroom_hum ):
+        print(" Lounge: " + lounge_temp + "C")
+        print("Bedroom: " + bedroom_temp + "C")
+
+        client.loop_stop()
+
 client = mqtt.Client()
+client.on_message = on_message
 
 parser = argparse.ArgumentParser()
 parser.add_argument("ip", help = "IP address of MQTT broker")
 args = parser.parse_args()
 
-broker_ip = args.ip
+broker_ip = str(args.ip)
 
+client.connect(broker_ip, 1883, 60)
+client.subscribe("home/#")
+
+client.loop_start()
+time.sleep(5)
