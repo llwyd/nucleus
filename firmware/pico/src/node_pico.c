9bd37eee9b2629e0dec2c8d966359d6e8406822b
llwyd <tom@llwyd.io>
2023-07-19 22:52:34 +0100

    basic state loop on pico

--- firmware/pico/src/node_pico.c
+++ firmware/pico/src/node_pico.c
@@ -5,15 +5,72 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
+#include "events.h"
+#include "fifo_base.h"
+#include "state.h"
+
+#define SIGNALS(SIG ) \
+    SIG( Tick ) \
+
+GENERATE_SIGNALS( SIGNALS );
+GENERATE_SIGNAL_STRINGS( SIGNALS );
+
+DEFINE_STATE( Idle );
+
+event_fifo_t events;
+
+static bool tick(struct repeating_timer *t)
+{
+    if( !FIFO_IsFull(&events.base) )
+    {
+        FIFO_Enqueue(&events, EVENT(Tick));
+    }
+    return true;
+}
+
+static state_ret_t State_Idle( state_t * this, event_t s )
+{
+    STATE_DEBUG(s);
+    state_ret_t ret = NO_PARENT(this);
+
+    switch(s)
+    {
+        case EVENT( Enter ):
+        case EVENT( Exit ):
+        case EVENT( Tick ):
+        {
+            ret = HANDLED();
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+}
 
 int main()
 {
     stdio_init_all();
+    struct repeating_timer timer;
+    
+    Events_Init(&events);
+    
+    state_t state_machine; 
+    state_machine.state = State_Idle; 
+    
+    FIFO_Enqueue( &events, EVENT(Enter) );
+
+    add_repeating_timer_ms(1000U, tick, NULL, &timer);
 
     while( true )
     {
-        printf("Hello, World!\n");
-        sleep_ms(1000U);
+        while( FIFO_IsEmpty( (fifo_base_t *)&events ) )
+        {
+            tight_loop_contents();
+        }
+        event_t e = FIFO_Dequeue( &events );
+        STATEMACHINE_Dispatch(&state_machine, e);
     }
     return 0;
 }
