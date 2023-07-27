2868aaa851ca3b11345ed6438b0a8a65f36edf8d
llwyd <tom@llwyd.io>
2023-07-27 22:39:18 +0100

    create emitter module for creating timer events

--- firmware/pico/src/node_pico.c
+++ firmware/pico/src/node_pico.c
@@ -11,6 +11,7 @@
 #include "fifo_base.h"
 #include "state.h"
 #include "wifi.h"
+#include "emitter.h"
 
 #define SIGNALS(SIG ) \
     SIG( Tick ) \
@@ -34,22 +35,6 @@ typedef struct
 }
 node_state_t;
 
-static event_fifo_t events;
-static critical_section_t crit;
-
-static bool Tick(struct repeating_timer *t)
-{
-    event_t event = (event_t)t->user_data;
-    critical_section_enter_blocking(&crit);
-    if( !FIFO_IsFull(&events.base) )
-    {
-        FIFO_Enqueue(&events, event);
-    }
-    
-    critical_section_exit(&crit);
-    return true;
-}
-
 static state_ret_t State_Idle( state_t * this, event_t s )
 {
     STATE_DEBUG(s);
@@ -61,9 +46,9 @@ static state_ret_t State_Idle( state_t * this, event_t s )
         case EVENT( Enter ):
         {
             WIFI_TryConnect();
-            add_repeating_timer_ms(500U, Tick, (void*)EVENT(Tick), node_state->timer);
-            add_repeating_timer_ms(1000U, Tick, (void*)EVENT(ReadSensor), node_state->read_timer);
-            add_repeating_timer_ms(5000U, Tick, (void*)EVENT(WifiCheckStatus), node_state->wifi_timer);
+            Emitter_Create(EVENT(Tick), node_state->timer, 500);
+            Emitter_Create(EVENT(ReadSensor), node_state->read_timer, 1000);
+            Emitter_Create(EVENT(WifiCheckStatus), node_state->wifi_timer, 5000);
             break;
         }
         case EVENT( ReadSensor ):
@@ -78,7 +63,7 @@ static state_ret_t State_Idle( state_t * this, event_t s )
             if( WIFI_CheckStatus() )
             {
                 printf("\tWifi Connected! :)\n");
-                cancel_repeating_timer(node_state->wifi_timer);
+                Emitter_Destroy(node_state->wifi_timer);
                 cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1); 
                 ret = HANDLED();
             }
@@ -86,8 +71,8 @@ static state_ret_t State_Idle( state_t * this, event_t s )
             {
                 printf("\tWifi Disconnected! :(\n");
                 WIFI_TryConnect();
-                cancel_repeating_timer(node_state->wifi_timer);
-                add_repeating_timer_ms(5000U, Tick, (void*)EVENT(WifiCheckStatus), node_state->wifi_timer);
+                Emitter_Destroy(node_state->wifi_timer);
+                Emitter_Create(EVENT(WifiCheckStatus), node_state->wifi_timer, 5000);
             }
             break;
         }
@@ -100,16 +85,19 @@ static state_ret_t State_Idle( state_t * this, event_t s )
 
 int main()
 {
+    event_fifo_t events;
+    critical_section_t crit;
+    
     struct repeating_timer timer;
     struct repeating_timer read_timer;
     struct repeating_timer wifi_timer;
     
     stdio_init_all();
     critical_section_init(&crit);
-
-    printf("--------------------------\n");    
     Enviro_Init(); 
     Events_Init(&events);
+    Emitter_Init(&events, &crit);
+    (void)WIFI_Init();
 
     node_state_t state_machine; 
     state_machine.state.state = State_Idle; 
@@ -118,8 +106,6 @@ int main()
     state_machine.wifi_timer = &wifi_timer;
     
     FIFO_Enqueue( &events, EVENT(Enter) );
-
-    (void)WIFI_Init();
 
     while( true )
     {
