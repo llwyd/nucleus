c834fc9a2fd1ccb2c86e96e8f2f9272e3d72b136
llwyd <tom@llwyd.io>
2023-08-06 00:40:07 +0100

    function to handle bunched up packets

--- firmware/pico/src/comms.c
+++ firmware/pico/src/comms.c
@@ -37,18 +37,33 @@ static err_t Recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err)
 
     for(; p != NULL; p = p->next )
     {
-        printf("\tCopying message len %d bytes\n\t", p->len);
-        for( uint32_t idx = 0; idx < p->len; idx++)
-        {
-            printf("0x%x,", ((uint8_t*)p->payload)[idx]);
-        }
-        printf("\b \n");
-        if( !FIFO_IsFull( &msg_fifo->base ) )
-        {
-            FIFO_Enqueue( msg_fifo, p->payload);
+        printf("\tMessage len %d bytes\n", p->len);
+        
+        /* Handle bunched together mqtt packets */
+        
+        uint8_t * msg_ptr = (uint8_t*)p->payload;
+        uint32_t size_to_copy = 0U;
 
-            /* Only emit event if the message was actually put in buffer */
-            Emitter_EmitEvent(EVENT(MessageReceived));
+        for( uint32_t idx = 0; idx < p->len; idx+=size_to_copy )
+        {
+            size_to_copy = *(msg_ptr + 1) + 2U;
+            printf("\tSize to copy: %d\n\t", size_to_copy);
+            
+            for( uint32_t jdx = 0; jdx < size_to_copy; jdx++)
+            {
+                printf("0x%x,", msg_ptr[jdx]);
+            }
+            printf("\b \n");
+
+            if( !FIFO_IsFull( &msg_fifo->base ) )
+            {
+                FIFO_Enqueue( msg_fifo, msg_ptr);
+
+                /* Only emit event if the message was actually put in buffer */
+                Emitter_EmitEvent(EVENT(MessageReceived));
+            }
+
+            msg_ptr += size_to_copy;
         }
     }
     
