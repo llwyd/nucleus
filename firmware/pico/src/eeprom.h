6cbb757124d607ff4b4403b91bfc2a5df39a4cdf
llwyd <tom@llwyd.io>
2023-09-19 23:01:20 +0100

    define eeprom interface

--- firmware/pico/src/eeprom.h
+++ firmware/pico/src/eeprom.h
@@ -1,6 +1,11 @@
 #ifndef EEPROM_H_
 #define EEPROM_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+
 extern void EEPROM_Test(void);
+extern void EEPROM_Write(uint8_t * buffer, uint16_t len);
+extern void EEPROM_Read(uint8_t * buffer, uint16_t len);
 
 #endif /* EEPROM_H_ */
