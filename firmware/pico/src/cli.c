7df6ce91ee249d3ff014ef0c517a877dfd736ffd
llwyd <tom@llwyd.io>
2023-09-22 23:18:35 +0100

    switch statement for handling control keys

--- firmware/pico/src/cli.c
+++ firmware/pico/src/cli.c
@@ -4,6 +4,7 @@
 #include "node_events.h"
 
 #define KEY_ENTER (0x0D)
+#define KEY_BACKSPACE (0x08)
 
 uint8_t * command_buffer;
 static uint8_t write_index;
@@ -13,23 +14,39 @@ static void rx_recv(void)
     while(uart_is_readable(uart0))
     {
         uint8_t latest_char = uart_getc(uart0);
-        if( latest_char != KEY_ENTER )
-        {
-            assert(write_index < CLI_CMD_SIZE);
 
-            /* -1 because the string needs to be null terminated*/
-            if(write_index < (CLI_CMD_SIZE - 1U))
-            {
-                printf("%c", latest_char);
-                command_buffer[write_index] = latest_char;
-                write_index++;
-            }
-        }
-        else
+        switch(latest_char)
         {
-            uart_puts(uart0, "\r\n");
-            write_index = (CLI_CMD_SIZE - 1U);
-            Emitter_EmitEvent(EVENT(HandleCommand));
+            case KEY_ENTER:
+            {
+                uart_puts(uart0, "\r\n");
+                write_index = (CLI_CMD_SIZE - 1U);
+                Emitter_EmitEvent(EVENT(HandleCommand));
+                break;
+            }
+            case KEY_BACKSPACE:
+            {
+                if(write_index > 0U)
+                {
+                    printf("\b \b%c", 0x1B);
+                    write_index--;
+                    command_buffer[write_index] = 0U;
+                }
+                break;
+            }
+            default:
+            {
+                assert(write_index < CLI_CMD_SIZE);
+
+                /* -1 because the string needs to be null terminated*/
+                if(write_index < (CLI_CMD_SIZE - 1U))
+                {
+                    printf("%c", latest_char);
+                    command_buffer[write_index] = latest_char;
+                    write_index++;
+                } 
+                break;
+            }
         }
     }
 }
