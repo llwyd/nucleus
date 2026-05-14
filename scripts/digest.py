7e718322d2a958d91cdc74ad956add08c7f1b5d6
llwyd <tom@llwyd.io>
2026-05-14 11:16:25 +0100

    send humidity data

--- scripts/digest.py
+++ scripts/digest.py
@@ -11,8 +11,10 @@ import base64
 from io import BytesIO
 
 def get_todays_data(db_path:str):
-    fig = Figure(figsize=(8,6))
-    ax = fig.subplots()
+    t_fig = Figure(figsize=(8,6))
+    h_fig = Figure(figsize=(8,6))
+    t_ax = t_fig.subplots()
+    h_ax = h_fig.subplots()
     
     engine = db.create_engine(db_path)
     conn = engine.connect()
@@ -39,22 +41,36 @@ def get_todays_data(db_path:str):
         data['datetime'] = pd.to_datetime(data['datestamp'] + " " + data['timestamp'])
         data = data[data.datetime > yesterdays_time]
         if not data.empty:
-            ax.plot(data['datetime'], np.float32(data['temperature']),label=node)
+            t_ax.plot(data['datetime'], np.float32(data['temperature']),label=node)
+            h_ax.plot(data['datetime'], np.float32(data['humidity']),label=node)
     
-    ax.set_title('Temperature',color='white')
-    ax.set_xlabel('Time', color='white')
-    ax.set_ylabel('Degrees (C)', color='white')
-    ax.tick_params('x', labelrotation=45, colors='white')
-    ax.tick_params('y', colors='white')
-    ax.spines[:].set_color('white')
-    ax.legend()
+    t_ax.set_title('Temperature',color='white')
+    t_ax.set_xlabel('Time', color='white')
+    t_ax.set_ylabel('Degrees (C)', color='white')
+    t_ax.tick_params('x', labelrotation=45, colors='white')
+    t_ax.tick_params('y', colors='white')
+    t_ax.spines[:].set_color('white')
+    t_ax.legend()
 
     date_format = mdates.DateFormatter('%H:%M')
-    ax.xaxis.set_major_formatter(date_format) 
+    t_ax.xaxis.set_major_formatter(date_format) 
+    
+    h_ax.set_title('Humidity',color='white')
+    h_ax.set_xlabel('Time',color='white')
+    h_ax.set_ylabel('Relative Humidity',color='white')
+    h_ax.tick_params('x', labelrotation=45, colors='white')
+    h_ax.tick_params('y', colors='white')
+    h_ax.spines[:].set_color('white')
+    h_ax.legend()
+    
+    h_ax.xaxis.set_major_formatter(date_format) 
    
-    buf = BytesIO()
-    fig.savefig(buf, format='png',transparent=True)
-    return base64.b64encode(buf.getbuffer()).decode("ascii")
+    t_buf = BytesIO()
+    t_fig.savefig(t_buf, format='png',transparent=True)
+    
+    h_buf = BytesIO()
+    h_fig.savefig(h_buf, format='png',transparent=True)
+    return base64.b64encode(t_buf.getbuffer()).decode("ascii"), base64.b64encode(h_buf.getbuffer()).decode("ascii")
 
 @click.command()
 @click.argument('key')
@@ -64,7 +80,7 @@ def get_todays_data(db_path:str):
 def digest(key:str, address:str, sender:str, path):
     db_path = f'sqlite:///{path}'
     click.echo(db_path)
-    graph = get_todays_data(db_path)
+    graph, h_graph = get_todays_data(db_path)
   
     url = 'https://api.smtp2go.com/v3/email/send'
     headers = {
@@ -79,7 +95,7 @@ def digest(key:str, address:str, sender:str, path):
                 'sender': f'{sender}',
                 'to': [f'{address}'],
                 'subject': 'Digest',
-                'html_body': f'<img src=\"data:image/png;base64, {graph}\"/>',
+                'html_body': f'<img src=\"data:image/png;base64, {graph}\"/><img src=\"data:image/png;base64, {h_graph}\"/>',
                 }
             )
 
